8c56b762d57c60c66437ce7ab6bda1a4eb0e0d8a
yosima050 <byosep050@gmail.com>
2024-11-01 07:48:07 +0700

    Percobaan 1

---
+++ ArrayBilanganModifikasi26.java
@@ -0,0 +1,16 @@
+public class ArrayBilanganModifikasi26 {
+
+    public static void main(String[] args) {
+        double[] bil = new double[4];
+
+        bil[0] = 5.0;
+        bil[1] = 12867;
+        bil[2] = 7.5;
+        bil[3] = 2000000;
+
+        System.out.println(bil[0]);
+        System.out.println(bil[1]);
+        System.out.println(bil[2]);
+        System.out.println(bil[3]);
+    }
+}
